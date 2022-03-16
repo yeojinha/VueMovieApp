41a5ac76460be1c8a018a963beefeed880e39e61
yeojinha <hayeojin4966@gmail.com>
2022-03-16 21:36:53 +0900

    영화검색

--- src/components/Headline.vue
+++ src/components/Headline.vue
@@ -8,8 +8,7 @@
       MOVIE DATABASE<br/>
     </h1>
     <p>
-    The OMDb API is a RESTful web service to obtain movie information, all content and images on the site are contributed and maintained by our users.<br/>
-    If you find this service useful, please consider making a one-time donation or become a patron.
+     영화검색 영화검색 영화검색 영화검색 영화검색 영화검색 영화검색 영화검색 영화검색 영화검색 영화검색 영화검색 영화검색 영화검색 영화검색 영화검색 
     </p>
   </div>
 </template>
