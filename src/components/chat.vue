2c9fa631383f53c27b7f6a97368ae2dd49c4ba45
yeojinha <hayeojin4966@gmail.com>
2022-04-15 00:06:59 +0900

    new heroku url connected

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -2,13 +2,14 @@
   <div class="chat-container">
     <header class="chat-header">
       <h1><i class="fas fa-smile"></i>영화-채팅-방</h1>
-    <!-- <a href="index.html" class="btn">Leave Room</a> -->
-    <button class="btn" @click="onClickleaveRoom">Leave Room</button>
+      <!-- <a href="index.html" class="btn">Leave Room</a> -->
+      <button class="btn" @click="onClickleaveRoom">Leave Room</button>
     </header>
     <main class="chat-main">
       <div class="chat-sidebar">
         <h3><i class="fas fa-comments"></i> Channel Name:</h3>
-        <h2 id="room-name">{{ this.channel }}</h2><!--TODO channel을 store에서 가져와야 함.-->
+        <h2 id="room-name">{{ this.channel }}</h2>
+        <!--TODO channel을 store에서 가져와야 함.-->
         <!-- <h3><i class="fas fa-users"></i> Users</h3>
         <ul id="users">
           <li v-for="user in this.$store.state.user.users" 
@@ -32,31 +33,41 @@
     </main>
     <div class="chat-form-container">
       <form id="chat-form">
-        <input id="msg" placeholder="Enter Message" required autocomplete="off" v-model="chatInputMessage" />
-        <button class="btn" @click="send"><i class="fas fa-paper-plane"></i> Send</button>
+        <input
+          id="msg"
+          placeholder="Enter Message"
+          required
+          autocomplete="off"
+          v-model="chatInputMessage"
+        />
+        <button class="btn" @click="send">
+          <i class="fas fa-paper-plane"></i> Send
+        </button>
       </form>
     </div>
   </div>
 </template>
 
 <script>
-import moment from 'moment'
+import moment from "moment";
 export default {
   created() {
-    this.channel = this.$route.query.channel || '';
+    this.channel = this.$route.query.channel || "";
     // this.$store.state.user.stateWebSocket= new WebSocket('ws://localhost:3000/');
-    this.$store.state.user.stateWebSocket = new WebSocket('ws://peaceful-ridge-59102.herokuapp.com');
-    this.websocket=this.$store.state.user.stateWebSocket;
-    
-    this.websocket.getUiqueID=function(){
+    this.$store.state.user.stateWebSocket = new WebSocket("ws://jonghwa220414.herokuapp.com/ws");
+    this.websocket = this.$store.state.user.stateWebSocket;
+
+    this.websocket.getUiqueID = function () {
       function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
       }
-      return s4() + s4() + '-' + s4();
+      return s4() + s4() + "-" + s4();
     };
 
     this.websocket.onmessage = ({ data }) => {
-      this.websocket.id = this.websocket.getUiqueID(); 
+      this.websocket.id = this.websocket.getUiqueID();
       const vo = JSON.parse(data);
       if (vo.channel === this.channel) {
         this.appendNewMessage(this.tempName, vo.message, vo.time);
@@ -64,38 +75,37 @@ export default {
     };
 
     this.websocket.onopen = (event) => {
-      console.log('open event..', event);
-      
+      console.log("open event..", event);
     };
 
     this.websocket.onerror = (event) => {
-      console.log('error', event);
+      console.log("error", event);
     };
 
-     this.websocket.onclose = (event) => {
-            console.log('close', event); 
+    this.websocket.onclose = (event) => {
+      console.log("close", event);
     };
-   
-  },data() {
+  },
+  data() {
     return {
-      tempName: 'Ghost',
-      user:{
-        id:null,
-        name:'',
-        room:''
+      tempName: "Ghost",
+      user: {
+        id: null,
+        name: "",
+        room: "",
       },
       // username:  this.$store.state.user.newUser.name,
       websocket: null,
-      channel: '',
-      chatInputMessage: '',
+      channel: "",
+      chatInputMessage: "",
     };
   },
   // computed:{
   //   getThisRoom(){
-      
+
   //   },
   //   userList(){
-  //     // console.log("this.$store.state.user.users: ",this.$store.state.user.users.filter(user=>user.room == this.channel)); 
+  //     // console.log("this.$store.state.user.users: ",this.$store.state.user.users.filter(user=>user.room == this.channel));
   //     // console.log("this. Get user: ", this.getUser);
   //     return this.$store.state.user.users.filter(user=>user.room == this.channel);
   //   },
@@ -109,61 +119,66 @@ export default {
   //   }
   // },
   methods: {
-   handleSend(){
-     console.log("websocket is? ",this.websocket)
-     console.log("websocket is on?",this.websocket===WebSocket.OPEN)
-     if(this.websocket===WebSocket.OPEN){
-       this.send()
-     }else if(this.websocket==WebSocket.CONNECTING){
-       this.websocket.addEventListener('open',()=>this.handleSend())
-     }
-   },
+    handleSend() {
+      console.log("websocket is? ", this.websocket);
+      console.log("websocket is on?", this.websocket === WebSocket.OPEN);
+      if (this.websocket === WebSocket.OPEN) {
+        this.send();
+      } else if (this.websocket == WebSocket.CONNECTING) {
+        this.websocket.addEventListener("open", () => this.handleSend());
+      }
+    },
     send() {
-      console.log("websocket: ",this.websocket);
-      console.log("state.websocket: ",this.$store.state.user.stateWebSocket)
-      console.log("this.websocket on Chat: ",this.websocket.readyState===WebSocket.OPEN);
-      if (this.chatInputMessage === '') return;
+      console.log("websocket: ", this.websocket);
+      console.log("state.websocket: ", this.$store.state.user.stateWebSocket);
+      console.log(
+        "this.websocket on Chat: ",
+        this.websocket.readyState === WebSocket.OPEN
+      );
+      if (this.chatInputMessage === "") return;
       const message = {
         channel: this.channel,
         message: this.chatInputMessage,
       };
       this.websocket.send(JSON.stringify(message));
-      this.chatInputMessage = '';
+      this.chatInputMessage = "";
     },
     onEnter() {
       this.send();
     },
     appendNewMessage(username, message, time) {
-      const div = document.createElement('div');
-      div.classList.add('message');
-      const p = document.createElement('p');
-      p.classList.add('meta');
+      const div = document.createElement("div");
+      div.classList.add("message");
+      const p = document.createElement("p");
+      p.classList.add("meta");
       p.innerText = username;
-      p.innerHTML += `<span>${moment(time).format('h:mm a')}</span>`;
+      p.innerHTML += `<span>${moment(time).format("h:mm a")}</span>`;
       div.appendChild(p);
-      const para = document.createElement('p');
-      para.classList.add('text');
+      const para = document.createElement("p");
+      para.classList.add("text");
       para.innerText = message;
       div.appendChild(para);
-      document.querySelector('.chat-messages').appendChild(div);
+      document.querySelector(".chat-messages").appendChild(div);
     },
     //TODO button router로 나가면, 다시 들어와서 메시지 치면 1개만 나와야 하는데, 나간 수 만큼 (모든 펑션) 작동함
-    onClickleaveRoom(event){
-        event.preventDefault();
-         if(this.$store.state.user.flag==true){
+    onClickleaveRoom(event) {
+      event.preventDefault();
+      if (this.$store.state.user.flag == true) {
+        this.websocket.close();
+        this.$store.state.user.flag = false;
+        console.log("flag on chat: ", this.$store.state.user.flag);
+        console.log(
+          "this.websocket.readyState: ",
+          this.websocket.readyState === WebSocket.OPEN
+        );
+      }
 
-           this.websocket.close();
-           this.$store.state.user.flag=false;
-           console.log("flag on chat: ",this.$store.state.user.flag)
-           console.log("this.websocket.readyState: ",this.websocket.readyState===WebSocket.OPEN)
-         }    
-         
-         this.$router.replace('/')
-    }
-  }
+      this.$router.replace("/");
+    },
+  },
 };
 </script>
 
 <style scoped>
-@import '../scss/chat.css';
+@import "../scss/chat.css";
 </style>
