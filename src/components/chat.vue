31a2b0dc07c41fac5803b78a45381fdfd698cb44
yeojinha <hayeojin4966@gmail.com>
2022-04-21 13:40:43 +0900

    add userList getters

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -12,7 +12,7 @@
         <!--TODO channel을 store에서 가져와야 함.-->
         <h3><i class="fas fa-users"></i> Users</h3>
         <ul id="users">
-          <li v-for="user in userList()" :key="user.id">
+          <li v-for="user in userList" :key="user.id">
             {{ user.name }}
           </li>
         </ul>
