3630d6e670e45cc23354a960e368a957c65d0b4a
yeojinha <hayeojin4966@gmail.com>
2022-04-22 16:57:49 +0900

    add list_of_users

--- src/store/user.js
+++ src/store/user.js
@@ -52,11 +52,11 @@ export default {
         name: user.name,
         room: user.room,
       });
-      vue.set("state.users", state.index, {
-        id: user.id,
-        name: user.name,
-        room: user.room,
-      });
+      // vue.set("state.users", state.index, {
+      //   id: user.id,
+      //   name: user.name,
+      //   room: user.room,
+      // });
       console.log("users array on users.js: ", state.users);
     },
     pullUser(state, user) {
