06f550533856d60968d1a6b161281650a3039a45
yeojinha <hayeojin4966@gmail.com>
2022-04-24 21:45:37 +0900

    add img

--- src/store/about.js
+++ src/store/about.js
@@ -6,7 +6,8 @@ export default {
       name: "Yeojin",
       email: "hayeojin@mail.com",
       phone: "+82-10-0000-0000",
-      image: "https://heropy.blog/css/images/logo.png",
+      image:
+        "https://lh3.googleusercontent.com/a-/AOh14GglqtyRMliBlT_0WhXhajGD4YB_CKoporw3RYOW3w=s360-p-rw-no",
     };
   },
 };
