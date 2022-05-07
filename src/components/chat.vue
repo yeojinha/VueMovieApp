bf638a2016a13594ee049dc6cc02b78ea55706d8
yeojinha <hayeojin4966@gmail.com>
2022-05-07 20:26:35 +0900

    User.new 없어어 추가

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -42,14 +42,10 @@ export default {
   created() {
     this.channel = this.$route.query.channel || "";
     this.websocket = this.$store.state.user.stateWebSocket;
-    console.log(
-      "this.$store.state.user.newUser: ",
-      this.$store.state.user.newUser
-    );
+
     this.websocket.onopen = ({ data }) => {
       //!! new User send to server
       this.websocket.send(JSON.stringify(this.$store.state.user.newUser));
-      console.log("this new User onopen: ", this.$store.state.user.newUser);
       const message = {
         name: "bot",
         message: `${this.chatUser.name}님 반갑습니다!`,
@@ -57,7 +53,6 @@ export default {
         bot: true,
         fresh: false,
       };
-      console.log("message on onopen: ", message);
       console.log("JSON.stringify(ms) onopen: ", JSON.stringify(message));
       if (this.websocket.send(JSON.stringify(message)) < 0) {
         console.log("안보내짐 error발생");
@@ -70,42 +65,35 @@ export default {
     this.websocket.onmessage = ({ data }) => {
       const vo = JSON.parse(data);
       //!!vo.fresh(새로운 사람 입장/퇴장만 하는 경우)
-      console.log("vo JSON 내용 체크 onmessage: ", vo);
       if (vo.fresh === true) {
         let User = {
           // id: vo.id,
           name: vo.name,
           room: vo.room,
           fresh: vo.true,
+          new: vo.new,
         };
-        console.log("vo.fresh 체크 작동함");
-        console.log(
-          " vo.fresh 체크 전 users on onmessage: ",
-          this.$store.state.user.users
-        );
-        console.log("User: ", User);
         //!! User가 리스트에 없다면 추가하는 것
         if (
           this.$store.state.user.users.find(
             (us) =>
-              us.name == User.name &&
-              ((us.room == User.room) == User.new) == true
-          ) == undefined
+              us.name == User.name && us.room == User.room && User.new == true
+          ) === undefined
         ) {
           this.$store.state.user.index++;
           //!! new User은 old
           User.new = false;
           this.$store.dispatch("user/userJoin", User);
-          console.log("users on onmessage: ", this.$store.state.user.users);
+          console.log("유저 리스트 추가 : ", this.$store.state.user.users);
           //!! User가 있는데, 요청이 들어온 것은 나가는 것이다.
         } else if (
           this.$store.state.user.users.find(
             (us) => us.name == User.name && us.room == User.room
-          )
+          ) !== undefined
         ) {
-          console.log("User가 있는데, 요청이 들어온 것은 나가는 것이다.");
           this.$store.state.user.index--;
           this.$store.dispatch("user/userLeave", User); //나가면 pull해줌
+          console.log("유저 퇴장 후 리스트 : ", this.$store.state.user.users);
         }
         //!vo.fresh가 false인 경우는 메시지인 경우
       } else if (vo.fresh !== true) {
@@ -158,12 +146,6 @@ export default {
   },
   methods: {
     send() {
-      console.log("websocket: ", this.websocket);
-      console.log("state.websocket: ", this.$store.state.user.stateWebSocket);
-      console.log(
-        "this.websocket on Chat: ",
-        this.websocket.readyState === WebSocket.OPEN
-      );
       if (this.chatInputMessage === "") return;
       const message = {
         //todo name추가해서 server에서 처음 접속시 메세지 전달.
@@ -173,7 +155,6 @@ export default {
         bot: false,
         fresh: false,
       };
-      console.log("this.USER-LIST on computed userList(): ", this.USER_LIST);
       console.log("message on chat: ", message);
       this.websocket.send(JSON.stringify(message));
       this.chatInputMessage = "";
@@ -220,17 +201,11 @@ export default {
         //!! this.chatUser가 old한 상태 server에 전달되게 하라.
         this.chatUser.new = false;
         this.websocket.send(JSON.stringify(this.chatUser));
-        console.log(
-          "send this.chatUser for leaving chnnel on event: ",
-          this.chatUser
-        );
         const message = {
           message: `${this.chatUser.name}님 안녕히가세요!`,
           channel: this.channel,
           bot: true,
         };
-        console.log("message on onclose: ", message);
-        console.log("JSON.stringify(ms) onclose: ", JSON.stringify(message));
         if (this.websocket.send(JSON.stringify(message)) < 0) {
           console.log("안보내짐 error발생");
         } else {
@@ -241,11 +216,6 @@ export default {
         }, 2000);
         localStorage.clear();
         this.$store.state.user.flag = false;
-        console.log("flag on chat: ", this.$store.state.user.flag);
-        console.log(
-          "this.websocket.readyState: ",
-          this.websocket.readyState === WebSocket.OPEN
-        );
       }
       this.$router.replace("/");
     },
